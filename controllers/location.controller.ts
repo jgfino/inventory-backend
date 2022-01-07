eeed80d49ce6b1dc66f1a1a20886d0922dd6c09f
Julia F <jgfino20@gmail.com>
2022-01-07 15:12:48 -0500

    move member logic to controller

--- controllers/location.controller.ts
+++ controllers/location.controller.ts
@@ -2,6 +2,8 @@ import LocationModel from "../schema/location.schema";
 import { authorizeAndCatchAsync, catchAsync } from "../error/catchAsync";
 import DatabaseErrors from "../error/errors/database.errors";
 import ItemModel from "../schema/item.schema";
+import UserModel from "../schema/user.schema";
+import AuthErrors from "../error/errors/auth.errors";
 
 /**
  * Create a location. Users can create one location on a free account and
@@ -147,23 +149,96 @@ export const deleteLocation = authorizeAndCatchAsync(
  * Free users can only add themselves to one Location at a time
  */
 export const addMember = catchAsync(async (req, res, next) => {
-  await LocationModel.addMember(req.user, req.params.id);
-  res
-    .status(200)
-    .send({ message: `Location ${req.params.id} updated successfully.` });
+  const userId = req.user._id;
+  const isPremium = await UserModel.verifySubscription(userId);
+
+  if (!isPremium && req.user.defaultSharedLocation) {
+    return next(
+      AuthErrors.PREMIUM_FEATURE(
+        "You must be a premium member to join more than one Location"
+      )
+    );
+  }
+
+  const owner: string = await LocationModel.findById(req.params.id, "owner", {
+    autopopulate: false,
+  }).distinct("owner")[0];
+  const ownerIsPremium = await UserModel.verifySubscription(owner);
+
+  if (!ownerIsPremium) {
+    return next(
+      AuthErrors.PREMIUM_FEATURE(
+        "The owner of this Location is not a Premium member. Have them upgrade in order to join this Location"
+      )
+    );
+  }
+
+  const updatedLocation = await LocationModel.updateOne(
+    { _id: req.params.id, owner: { $ne: userId } },
+    {
+      $addToSet: {
+        members: userId,
+        notificationDays: { user: userId },
+      },
+    }
+  );
+
+  if (updatedLocation.matchedCount < 1) {
+    return next(
+      DatabaseErrors.NOT_FOUND(
+        "A location with this id does not exist or you do not have permission to add this user to it."
+      )
+    );
+  }
+
+  if (!req.user.defaultSharedLocation) {
+    await UserModel.updateOne(
+      { _id: userId },
+      { defaultSharedLocation: req.params.id }
+    );
+  }
 });
 
 /**
  * Remove a member from a location
  */
-export const removeMember = catchAsync(async (req, res, next) => {
-  await LocationModel.removeMember(
-    req.user,
-    req.params.id,
-    req.params.memberId
-  );
+export const removeMember = authorizeAndCatchAsync(
+  async (req, res, next, locationModel) => {
+    const updatedLocation = await locationModel.updateOne(
+      { _id: req.params.id },
+      {
+        $pull: {
+          members: req.params.memberId,
+          notificationDays: { user: req.params.memberId },
+        },
+      }
+    );
 
-  res
-    .status(200)
-    .send({ message: `Location ${req.params.id} updated successfully.` });
-});
+    if (updatedLocation.matchedCount < 1) {
+      return next(
+        DatabaseErrors.NOT_FOUND(
+          "A location with this id does not exist or you do not have permission to modify it."
+        )
+      );
+    }
+
+    // Delete items in this location owned by the removed member
+    await ItemModel.deleteMany({
+      owner: req.params.memberId,
+      location: req.params.id,
+    });
+
+    // Update the user's default shared location, if necessary
+    if (req.user.defaultSharedLocation.toString() == req.params.id) {
+      await UserModel.updateOne(
+        { _id: req.params.memberId },
+        { $unset: { defaultSharedLocation: "" } }
+      );
+    }
+
+    res
+      .status(200)
+      .send({ message: `Location ${req.params.id} updated successfully.` });
+  },
+  [LocationModel, "update"]
+);
